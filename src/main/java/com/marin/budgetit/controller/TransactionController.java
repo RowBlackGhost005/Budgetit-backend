5fe8284828a1ddc111e8f903b96076fc6dc38056
Luis Angel Marin <la.marin@outlook.com>
2025-11-10 20:20:52 -0700

    Base project

---
+++ src/main/java/com/marin/budgetit/controller/TransactionController.java
@@ -0,0 +1,15 @@
+package com.marin.budgetit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @GetMapping("/")
+    public String findTransactions() {
+        return "This is a transaction test!";
+    }
+}
